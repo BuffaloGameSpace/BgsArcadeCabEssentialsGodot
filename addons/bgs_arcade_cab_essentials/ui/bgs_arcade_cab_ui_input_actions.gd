248da705cf3ba17f147e4d06f170fa8f3d50dbed
Christopher Ray Langford <chris@hellocld.com>
2024-02-20 14:55:30 -0500

    Tree edits for actions, not yet functioning

--- addons/bgs_arcade_cab_essentials/ui/bgs_arcade_cab_ui_input_actions.gd
+++ addons/bgs_arcade_cab_essentials/ui/bgs_arcade_cab_ui_input_actions.gd
@@ -5,18 +5,6 @@ extends Control
 @export var input_action_tree:Tree
 
 var _action_tree_root:TreeItem
-var _up_root:TreeItem
-var _left_root:TreeItem
-var _right_root:TreeItem
-var _down_root:TreeItem
-var _a_root:TreeItem
-var _b_root:TreeItem
-var _c_root:TreeItem
-var _x_root:TreeItem
-var _y_root:TreeItem
-var _z_root:TreeItem
-var _start_root:TreeItem
-var _insert_credit_root:TreeItem
 
 
 func _ready() -> void:
@@ -25,6 +13,7 @@ func _ready() -> void:
 
 func _setup_action_tree() -> void:
 	_action_tree_root = input_action_tree.create_item()
+	
 	
 
 func _show_add_action_window(input_button) -> void:
