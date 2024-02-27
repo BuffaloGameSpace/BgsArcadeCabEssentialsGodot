6e85d6c536f9d660945a990e8448d502a473ea62
Christopher Ray Langford <chris@hellocld.com>
2024-02-27 09:07:28 -0500

    Add left stick to directional input mapping

--- demo/src/input_test/cab_stick.gd
+++ demo/src/input_test/cab_stick.gd
@@ -18,24 +18,15 @@ var _sprites = {
 var _pos: Vector2i
 
 
-func _unhandled_input(event):
-	if event.is_action_pressed("bgs_left_p%d" % player):
-		_pos.x -= 1
-	if event.is_action_released("bgs_left_p%d" % player):
-		_pos.x += 1
-	if event.is_action_pressed("bgs_right_p%d" % player):
-		_pos.x += 1
-	if event.is_action_released("bgs_right_p%d" % player):
-		_pos.x -= 1
-	if event.is_action_pressed("bgs_up_p%d" % player):
-		_pos.y -= 1
-	if event.is_action_released("bgs_up_p%d" % player):
+func _process(_delta):
+	_pos = Vector2i.ZERO
+	if Input.is_action_pressed("bgs_down_p%d" % player):
 		_pos.y += 1
-	if event.is_action_pressed("bgs_down_p%d" % player):
-		_pos.y += 1
-	if event.is_action_released("bgs_down_p%d" % player):
+	if Input.is_action_pressed("bgs_up_p%d" % player):
 		_pos.y -= 1
-	
+	if Input.is_action_pressed("bgs_left_p%d" % player):
+		_pos.x -= 1
+	if Input.is_action_pressed("bgs_right_p%d" % player):
+		_pos.x += 1
 	if _sprites.has(_pos):
 		texture = _sprites[_pos]
-
